1897117d0befbb50bfa7fc8921360abc5dad26dc
unknown <sanidhyakhamkar313@gmail.com>
2025-08-05 11:51:37 +0530

    Added

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "bucket_name" {
-  value = aws_s3_bucket.raw_backup_bucket.bucket
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.raw_bucket.id
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "aws_region" {
-  description = "AWS region to deploy S3 bucket"
+  description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
 variable "bucket_name" {
-  description = "Name of the S3 bucket"
+  description = "S3 bucket name"
   type        = string
+  default     = "my-terraform-bucket-yogesh-khamkar-202508051133"
 }
