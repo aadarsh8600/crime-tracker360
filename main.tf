347d752574612f74a1455c21d05a0cb4a93c222a
yogeshkerkar48 <56548865+yogeshkerkar48@users.noreply.github.com>
2025-08-07 13:45:07 +0530

    add two crawlers for facts and dimensions

--- main.tf
+++ main.tf
@@ -25,14 +25,24 @@ resource "aws_glue_job" "etl_job" {
   worker_type       = "G.1X"
 }
 
-resource "aws_glue_crawler" "etl_crawler" {
-  name          = var.glue_crawler_name
+resource "aws_glue_crawler" "facts_crawler" {
+  name          = var.glue_facts_crawler_name
   role          = local.glue_role_arn
   database_name = aws_glue_catalog_database.etl_db.name
 
   s3_target {
-    path = "s3://${aws_s3_bucket.etl_bucket.bucket}/cleaned_data/"
+    path = "s3://${aws_s3_bucket.etl_bucket.bucket}/facts_data/"
  }
 
   depends_on = [aws_glue_job.etl_job]
+}
+resource "aws_glue_crawler" "dimension_crawler"{
+  name=var.glue_dimensions_crawler_name
+  role=local.glue_role_arn
+  database_name=aws_glue_catalog_database.etl_db_name
+  s3_target {
+    path="s3://${aws_s3_bucket.etl_bucket.bucket}/dimensions_data/"
+}
+
+  depends_on=[aws_glue_job.etl_job]
 }
